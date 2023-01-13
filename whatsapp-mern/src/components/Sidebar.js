0b3d6d89b52ec83c60d17fb49e622f0b36a50750
Naba Kr Missong <mrmissong@gmail.com>
2023-01-13 22:25:51 +0530

    mandatory file saving'

---
+++ whatsapp-mern/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sidebar.css";
+function Sidebar() {
+	return (
+		<div className="sidebar">
+			Sidebar
+			<div className="sidebar_header">
+				<div className="sidebar_hederRight"></div>
+			</div>
+		</div>
+	);
+}
+
+export default Sidebar;
