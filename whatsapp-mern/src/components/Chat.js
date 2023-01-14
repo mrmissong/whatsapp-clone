b5c319bfd60ac74761a866665587465e95039a93
Naba Kr Missong <mrmissong@gmail.com>
2023-01-14 16:23:45 +0530

    minimal front-end

--- whatsapp-mern/src/components/Chat.js
+++ whatsapp-mern/src/components/Chat.js
@@ -1,7 +1,73 @@
+import { Avatar, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import React from "react";
 import "./Chat.css";
+import MoreVert from "@mui/icons-material/MoreVert";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import MicIcon from "@mui/icons-material/Mic";
 function Chat() {
-	return <div className="chat">Chat</div>;
+	return (
+		<div className="chat">
+			<div className="chat_header">
+				<Avatar />
+				<div className="chat_headerInfo">
+					<h3>Room name</h3>
+					{/* <p>last seen </p> */}
+				</div>
+				<div className="chat_headerRight">
+					<IconButton>
+						<SearchIcon />
+					</IconButton>
+					<IconButton>
+						<MoreVert />
+					</IconButton>
+				</div>
+			</div>
+			<div className="chat_body">
+				<p className="chat_message">
+					<span className="chat_name">Dog</span>
+					this is a message
+					<span className="chat_timestamp">{new Date().toUTCString()}</span>
+				</p>
+				<p className="chat_message">
+					<span className="chat_name">Dog</span>
+					this is a message
+					<span className="chat_timestamp">{new Date().toUTCString()}</span>
+				</p>
+				<p className="chat_message chat_receiver">
+					<span className="chat_name">Naba</span>
+					this is a message
+					<span className="chat_timestamp">{new Date().toUTCString()}</span>
+				</p>
+				<p className="chat_message">
+					<span className="chat_name">Dog</span>
+					this is a message
+					<span className="chat_timestamp">{new Date().toUTCString()}</span>
+				</p>
+				<p className="chat_message chat_receiver">
+					<span className="chat_name">Naba</span>
+					this is a message
+					<span className="chat_timestamp">{new Date().toUTCString()}</span>
+				</p>
+			</div>
+			<div className="chat_footer">
+				<IconButton>
+					<InsertEmoticonIcon />
+				</IconButton>
+				<IconButton>
+					<AttachFileIcon />
+				</IconButton>
+				<form>
+					<input type="text" placeholder="Type a message"></input>
+					<button type="submit">send</button>
+				</form>
+				<IconButton>
+					<MicIcon />
+				</IconButton>
+			</div>
+		</div>
+	);
 }
 
 export default Chat;
--- whatsapp-mern/src/components/Sidebar.js
+++ whatsapp-mern/src/components/Sidebar.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Sidebar.css";
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
 import GroupsIcon from "@mui/icons-material/Groups";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import ChatIcon from "@mui/icons-material/Chat";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Avatar, IconButton } from "@mui/material";
+import SidebarChat from "./SidebarChat";
+import FilterListIcon from "@mui/icons-material/FilterList";
 
 function Sidebar() {
 	return (
 		<div className="sidebar">
-			Sidebar
 			<div className="sidebar_header">
 				<Avatar src="https://i.imgur.com/ONxzjfl.jpg" />
 				<div className="sidebar_hederRight">
@@ -28,7 +30,21 @@ function Sidebar() {
 				</div>
 			</div>
 			<div className="sidebar_search">
-				<div></div>
+				<div className="sidebar_searchContainer">
+					<IconButton>
+						<SearchOutlined />
+					</IconButton>
+
+					<input placeholder="search or start new chat" type="text"></input>
+					<IconButton>
+						<FilterListIcon />
+					</IconButton>
+				</div>
+			</div>
+			<div className="sidebar_chats">
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
 			</div>
 		</div>
 	);
