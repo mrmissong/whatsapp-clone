b5fd241784844cf9bf5d1cef4108d3716faf4d55
mrmissong <mrmissong@gmail.com>
2023-04-05 23:12:31 +0530

    backend url

--- whatsapp-mern/src/axios.js
+++ whatsapp-mern/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "http://localhost:9000",
+	baseURL: "https://whatsapp-clone-backend-neln.onrender.com",
 });
 
 export default instance;
